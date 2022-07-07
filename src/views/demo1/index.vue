91ee29b25ed81484f810f09272df7b6850430945
zhangjf26 <zhangjf26@lenovo.com>
2022-07-07 13:52:56 +0800

    解决sass-loader版本问题

--- src/views/demo1/index.vue
+++ src/views/demo1/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <!-- 挖掘组件传值 -->
+  <div>
+    <!-- 挖掘组件传值 -->
   <div>Component value transmission</div>
+  <!-- 父组件传子组件 -->
+  <div>我是父=======================</div>
+  </div>
 </template>
 <script src="./script.js"></script>
 <style lang="scss" scoped src="./style.scss"></style>
